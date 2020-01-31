24cdd180ae879fd2081ff32c8f97034a09df44d0
rony cb <mylovepre16@gmail.com>
2020-01-31 13:40:20 -0500

    prueba de conflicto

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,3 @@
 export { default as NotFound } from './NotFound';
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
-export { default as Inventario } from './Inventario/';
